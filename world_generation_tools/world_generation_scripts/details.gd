62c02a9278b512a6469dc988c5378a6308736540
Agamess <115094566+Agamess@users.noreply.github.com>
2023-07-18 22:39:18 +0200

    Add files via upload

---
+++ world_generation_tools/world_generation_scripts/details.gd
@@ -0,0 +1,102 @@
+@tool
+extends Node3D
+
+@export var Delete:bool : set=delete
+@export var Generate_stuff_in_editor:bool : set=generate_stuff
+@export var Generate_stuff_on_start:bool
+@export var rocks: Array[PackedScene]
+@export var rock_count = 50
+@export var trees: Array[PackedScene]
+@export var tree_count = 150
+@export var flowers: Array[PackedScene]
+@export var flower_count = 100
+@onready var tarrain = $tarrain
+@export var world_size = 3000
+@export var max_spawn_height = 100000
+@export var min_spawn_height = 0.0
+
+
+var MIN_X=-world_size/2
+var MAX_X=world_size/2
+var MIN_Z=-world_size/2
+var MAX_Z=world_size/2
+#const MAX_HEIGHT = 20
+
+var stuff_placed = false
+var complete_land = false
+var offset = Vector3(0,2.5,0)
+
+func _ready():
+	seed(randi())
+	if Generate_stuff_on_start:
+		generate_stuff(0)
+
+
+func delete(__):
+	for i in get_children():
+		i.queue_free()
+
+
+func generate_stuff(__):
+	stuff_placed = false
+	complete_land = true
+	
+	#generate deatails
+	if complete_land and stuff_placed == false:
+		generate_trees()
+		generate_rocks()
+		generate_flowers()
+		stuff_placed = true
+
+
+func generate_trees():
+	for i in tree_count:
+		var num = randi_range(0,rocks.size()-1)
+		var treei = trees[num].instantiate()
+		add_child(treei)
+		var pos = Vector3(randi_range(MIN_Z,MAX_X), max_spawn_height, randi_range(MIN_Z,MAX_X))
+		var fnl_pos = get_down_ray(pos)
+		if fnl_pos == Vector3.ZERO:
+			treei.queue_free()
+		var rot = Vector3(0, randi_range(0,360), 0)
+		treei.global_transform.origin = fnl_pos
+		treei.rotation_degrees = rot
+
+
+func generate_rocks():
+	for i in rock_count:
+		var num = randi_range(0,rocks.size()-1)
+		var rocki = rocks[num].instantiate()
+		add_child(rocki)
+		var pos = Vector3(randi_range(MIN_Z,MAX_X), max_spawn_height, randi_range(MIN_Z,MAX_X))
+		var fnl_pos = get_down_ray(pos)
+		if fnl_pos == Vector3.ZERO:
+			rocki.queue_free()
+		var rot = Vector3(0, randi_range(0,360), 0)
+		rocki.global_transform.origin = fnl_pos
+		rocki.rotation_degrees = rot
+
+
+func generate_flowers():
+	for i in flower_count:
+		var num = randi_range(0,rocks.size()-1)
+		var floweri = flowers[num].instantiate()
+		add_child(floweri)
+		var pos = Vector3(randi_range(MIN_Z,MAX_X), max_spawn_height, randi_range(MIN_Z,MAX_X))
+		var fnl_pos = get_down_ray(pos)
+		if fnl_pos == Vector3.ZERO:
+			floweri.queue_free()
+		var rot = Vector3(0, randi_range(0,360), 0)
+		floweri.global_transform.origin = fnl_pos
+		floweri.rotation_degrees = rot
+
+
+func get_down_ray(pos):
+	var lenght = max_spawn_height - min_spawn_height;
+	var query = PhysicsRayQueryParameters3D.create(pos, pos + Vector3(0, -lenght, 0))
+	var collision = get_world_3d().direct_space_state.intersect_ray(query)
+	if collision:
+		return collision.position
+	else:
+		
+		return Vector3.ZERO
---
+++ world_generation_tools/world_generation_scripts/tarrain.gd
@@ -0,0 +1,49 @@
+@tool
+extends StaticBody3D
+
+@export var Generate_mesh:bool : set=generate_mesh
+@export var size:int = 3000
+@export var fall_width:int = 10
+@export var subdivide:int = 63
+@export var amplitude:int = 163
+@export var max_amplitude_clamp:int = 10000
+@export var min_amplitude_clamp:int = -32
+@export var terrain_height:int = 100
+@export var noice_frequency:float = 0.001
+@export var noise:FastNoiseLite = FastNoiseLite.new()
+@export var amplitude_curve:Curve
+@export var terrain_height_curve:Curve
+
+func _ready() -> void:
+	#generate_mesh()
+	pass # Replace with function body.
+	
+func generate_mesh(__) -> void:
+	print("Generating Mesh...")
+	
+	noise.seed = randi()
+	
+	var plane_mesh = PlaneMesh.new()
+	plane_mesh.size = Vector2(size,size)
+	plane_mesh.subdivide_depth = subdivide
+	plane_mesh.subdivide_width = subdivide
+	
+	var surface_tool = SurfaceTool.new()
+	surface_tool.create_from(plane_mesh,0)
+	var data = surface_tool.commit_to_arrays()
+	var vertices = data[ArrayMesh.ARRAY_VERTEX]
+	
+	for i in vertices.size():
+		var vertex = vertices[i]
+		var distance = 1-clamp(Vector2(vertex.x,vertex.z).distance_to(Vector2(0,0))/(size*0.5),0,1)
+		vertices[i].y = clamp((noise.get_noise_2d(vertex.x,vertex.z) * (amplitude * amplitude_curve.sample(distance))),min_amplitude_clamp,max_amplitude_clamp) + ((terrain_height_curve.sample(distance) - 0.5) * terrain_height)
+	data[ArrayMesh.ARRAY_VERTEX] = vertices
+		
+	var array_mesh = ArrayMesh.new()
+	array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES,data)
+	
+	surface_tool.create_from(array_mesh,0)
+	surface_tool.generate_normals()
+
+	$MeshInstance3D.mesh = surface_tool.commit()
+	$CollisionShape3D.shape = array_mesh.create_trimesh_shape()
